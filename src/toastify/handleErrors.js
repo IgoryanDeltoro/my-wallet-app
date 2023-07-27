7b895ba98ef2744e7af09d2f304e3162eded2463
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-07-27 16:42:59 +0300

    ADDED: handling errors

---
+++ src/toastify/handleErrors.js
@@ -0,0 +1,20 @@
+import { toast } from "react-toastify";
+
+export const handleErrors = (error) => {
+  switch (error?.code) {
+    case 4001:
+      toast.error(error?.message);
+      break;
+    case -32602:
+      toast.error(error?.message);
+      break;
+    case -32603:
+      toast.error(error?.message);
+      break;
+    case -32000:
+      toast.error("Insufficient funds");
+      break;
+    default:
+      break;
+  }
+};
