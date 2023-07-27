3a0e190823b2c4a17f241f4bea75aa6d6e86a20e
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-07-27 16:36:44 +0300

    REFACTORED

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { FormFields, Header } from "./components";
+
 const App = () => {
   return (
     <>
-      <h1>My wallet</h1>
+      <Header />
+      <FormFields />
+      <ToastContainer />
     </>
   );
 };
