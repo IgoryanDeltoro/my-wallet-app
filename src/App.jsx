609fb9e259a8e85717c0811a0307a932791c56b1
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-07-30 13:23:20 +0300

    RETURNED: css imports to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { FormFields, Header } from "./components";
 
 const App = () => {
