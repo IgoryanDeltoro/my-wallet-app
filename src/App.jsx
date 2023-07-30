4756b10b96c7be7937dc8d08a0457e033bde5cf1
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-07-30 13:58:35 +0300

    MOVED: css of bootstrap  import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { FormFields, Header } from "./components";
