15b5ae8b6c3f66e7f52eee0f89ab78f8940eccfd
Igor Bondarchuk <bondatchuk1989@gmail.com>
2023-07-30 13:10:34 +0300

    CHANGED: import bootstrap style sheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 import { FormFields, Header } from "./components";
 
 const App = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { store } from "./redux/store.js";
 import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
